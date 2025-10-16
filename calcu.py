2cfe7439d4c732a9202b0b44cd937787f324b07a
Eva Bordon <eabordon@itel.edu.ar>
2025-10-16 20:01:45 -0300

    Calculadora final (Commit 4)

--- calcu.py
+++ calcu.py
@@ -39,10 +39,25 @@ def factorial(a):
     else:
         return "No se puede calcular factorial de número negativo o no entero"
 
+def promedio(lista):
+    if len(lista) > 0:
+        return sum(lista) / len(lista)
+    else:
+        return "La lista está vacía"
+
+def cm_a_pulgadas(cm):
+    return cm / 2.54
+
+def celsius_a_fahrenheit(c):
+    return (c * 9/5) + 32
+
 num1 = 5
 num2 = 2
+lista_numeros = [4, 8, 15, 16, 23, 42]
+cm = 10
+celsius = 25
 
-print("Calculadora extendida")
+print("Calculadora completa")
 print("Suma:", suma(num1, num2))
 print("Resta:", resta(num1, num2))
 print("Multiplicación:", multiplicacion(num1, num2))
@@ -52,3 +67,6 @@ print("Radicación:", radicacion(num1, num2))
 print("Porcentaje:", porcentaje(num1, num2))
 print("Módulo:", modulo(num1, num2))
 print(f"Factorial de {num1}:", factorial(num1))
+print("Promedio de la lista:", promedio(lista_numeros))
+print(f"{cm} cm son {cm_a_pulgadas(cm)} pulgadas")
+print(f"{celsius}°C son {celsius_a_fahrenheit(celsius)}°F")
